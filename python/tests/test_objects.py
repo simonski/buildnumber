e2dab6b5b754591de93ff77a9cd4d983e8e98145
johntelforduk <jtelford1@gmail.com>
2019-10-12 17:06:21 +0100

    Added more test cases.

--- python/tests/test_objects.py
+++ python/tests/test_objects.py
@@ -5,7 +5,7 @@ from buildnumber import objects, utils
 TEST_FILENAME = "test-data/Buildfile"
 
 
-class BuildnumberTest(unittest.TestCase):
+class BuildNumberTest(unittest.TestCase):
     @classmethod
     def tearDownClass(self):
         pass
@@ -22,34 +22,49 @@ class BuildnumberTest(unittest.TestCase):
 
     def test_a_thing(self):
         bn = objects.Buildfile(TEST_FILENAME)
-        self.assertEquals("0.0.1", bn.get("app1"))
+        self.assertEqual("0.0.1", bn.get("app1"))
         bn.increment("app1")
-        self.assertEquals("0.0.2", bn.get("app1"))
+        self.assertEqual("0.0.2", bn.get("app1"))
         bn.increment("app1")
         bn.increment("app1")
-        self.assertEquals("0.0.4", bn.get("app1"))
+        self.assertEqual("0.0.4", bn.get("app1"))
         bn.increment("app1", "revision")
-        self.assertEquals("0.0.5", bn.get("app1"))
+        self.assertEqual("0.0.5", bn.get("app1"))
         bn.increment("app1", "minor")
-        self.assertEquals("0.1.0", bn.get("app1"))
+        self.assertEqual("0.1.0", bn.get("app1"))
         bn.increment("app1", "minor")
-        self.assertEquals("0.2.0", bn.get("app1"))
+        self.assertEqual("0.2.0", bn.get("app1"))
         bn.increment("app1", "revision")
-        self.assertEquals("0.2.1", bn.get("app1"))
+        self.assertEqual("0.2.1", bn.get("app1"))
         bn.increment("app1", "revision")
         bn.increment("app1", "revision")
         bn.increment("app1", "revision")
-        self.assertEquals("0.2.4", bn.get("app1"))
+        self.assertEqual("0.2.4", bn.get("app1"))
         bn.increment("app1", "major")
-        self.assertEquals("1.0.0", bn.get("app1"))
+        self.assertEqual("1.0.0", bn.get("app1"))
 
-        app2_version = bn.get("app2")
-        self.assertEquals("0.0.2", app2_version)
+        # Test a second app's version numbering.
+        self.assertEqual("0.0.2", bn.get("app2"))
+        bn.increment("app2")
+        self.assertEqual("0.0.3", bn.get("app2"))
+        self.assertEqual("1.0.0", bn.get("app1"))  # Check incrementing one app's version doesn't mess up another one.
 
-        app3_version = bn.get("app3")
-        self.assertEquals("0.0.3", app3_version)
+        # Test a third apps' version numbering.
+        self.assertEqual("0.0.3", bn.get("app3"))
+        bn.increment("app3", "minor")
+        self.assertEqual("0.1.0", bn.get("app3"))
+        bn.increment("app3", "major")
+        self.assertEqual("1.0.0", bn.get("app3"))
+        self.assertEqual("1.0.0", bn.get("app1"))  # Check incrementing one app's version doesn't mess up another one.
 
-        app3_version = bn.get("app4")
-        self.assertEquals("34", app3_version)
+        # Test an integer version number.
+        self.assertEqual("34", bn.get("app4"))
+        bn.increment("app4", "major")
+        self.assertEqual("35", bn.get("app4"))
+        bn.increment("app4", "minor")
+        self.assertEqual("36", bn.get("app4"))
+        bn.increment("app4", "revision")
+        self.assertEqual("37", bn.get("app4"))
+        self.assertEqual("1.0.0", bn.get("app1"))  # Check incrementing one app's version doesn't mess up another one.
 
-        self.assertEquals("0.0.0", bn.get("no-app"))
+        self.assertEqual("0.0.0", bn.get("no-app"))
